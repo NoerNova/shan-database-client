c7012c7e16d083e8652a0b8ce9de8a44a045de28
shannews <noernova666@gmail.com>
2022-08-02 12:02:21 +0000

    update files list icon thumbnail

--- src/components/search-result/search-result.tsx
+++ src/components/search-result/search-result.tsx
@@ -6,12 +6,14 @@ import {
   noSearchResult,
 } from "recoil-state/state";
 import { useMantineColorScheme, Loader, Pagination } from "@mantine/core";
-import { usePagination } from "@mantine/hooks";
 import { indexPropsType } from "../search-box/searchIndex";
 import { dateFormat } from "utils/date";
 import { Suspense } from "react";
 
+import { suffix } from "../search-box/suffix";
+
 import ViewportList from "react-viewport-list";
+import { forEach, indexOf } from "lodash";
 
 const DisplaySearchResult: React.FC = () => {
   const resultList = useRecoilValue<indexPropsType[]>(searchResultState);
@@ -40,11 +42,62 @@ const DisplaySearchResult: React.FC = () => {
     "https://shannews.org/wp-content/uploads/2021/05/Shan-Logo-used-2018-1_Optimize.png";
 
 
-  const getDefaultThumbnail = (type: string) => {
-    
+  const getThumbnailFromType = (type: string) => {
+    let typeInclude = "";
+    let icon = "src/assets/filetypes-pack"
+
+    forEach(suffix, (s) => {
+      if (s.includes(type)) {
+        typeInclude = Object.keys(suffix)[Object.values(suffix).indexOf(s)];
+      }
+    })
+
+    switch (typeInclude) {
+      case "Compress":
+        return `${icon}/zip.png`
+      case "Audios":
+        return `${icon}/music.png`
+      case "Documents":
+        return `${icon}/text.png`
+      case "Fonts":
+        return `${icon}/font-file.png`
+      case "Images":
+        return `${icon}/image.png`
+      case "Videos":
+        return `${icon}/video.png`
+      default:
+        return defaultImageLogo
+    }
   }
 
-  const getImageThumbnail = (path: string) => {
+  const getDefaultThumbnail = (type: string) => {
+    let icon = "src/assets/filetypes-pack"
+
+    switch (type) {
+      case "doc":
+      case "docx":
+        return `${icon}/doc.png`
+      case "txt":
+        return `${icon}/txt.png`
+      case "xls":
+      case "xlsx":
+        return `${icon}/xls.png`
+      case "psd":
+        return `${icon}/psd.png`
+      case "ai":
+        return `${icon}/adobe-illustrator.png`
+      case "pdf":
+        return `${icon}/pdf.png`
+      case "ppt":
+        return `${icon}/ppt.png`
+      case "svg":
+        return `${icon}/svg.png`
+      default:
+        return getThumbnailFromType(type);
+    } 
+  }
+
+  const getThumbnailFromPath = (path: string) => {
     const sid = import.meta.env.VITE_TEST_SID;
     const thumbURL = import.meta.env.VITE_IMAGE_THUMBNAIL_URL;
 
@@ -55,6 +108,18 @@ const DisplaySearchResult: React.FC = () => {
     }&name=${image_name_path![1]}`;
     
     return image_thumbnail_url;
+  }
+
+  const getImageThumbnail = (type: string, path: string) => {
+    switch (type.toLowerCase()) {
+      case "png":
+      case "jpg":
+      case "jpeg":
+      case "gif":
+        return getThumbnailFromPath(path)
+      default:
+        return getDefaultThumbnail(type)
+    }
   };
 
   return (
@@ -83,7 +148,7 @@ const DisplaySearchResult: React.FC = () => {
                   >
                     <div className="flex flex-col w-50 h-50 justify-center items-center mr-10">
                       <a href="#" className="block relative">
-                        <object data={getImageThumbnail(currentList.path)} className="w-40 h-40 object-scale-down flex justify-center items-center">
+                        <object data={getImageThumbnail(currentList.type, currentList.path)} type="image/jpg" className="w-40 h-40 object-scale-down flex justify-center items-center">
                           <img alt="thumb_nail" src={defaultImageLogo} loading='lazy'/>
                         </object>
                       </a>
